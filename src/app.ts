4007b08caef7769c6e6ca4fcee438ce98376f1bf
MeganRook18 <megan-rook@malone-digital.com>
2020-06-11 11:56:03 +0100

    added actions, store subscription. added reducers

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,54 @@
-import { renderTodos } from './utils';
+import { renderTodos } from "./utils";
+import * as fromStore from "./store";
 
-const input = document.querySelector('input') as HTMLInputElement;
-const button = document.querySelector('button') as HTMLButtonElement;
-const destroy = document.querySelector('.unsubscribe') as HTMLButtonElement;
-const todoList = document.querySelector('.todos') as HTMLLIElement;
+const input = document.querySelector("input") as HTMLInputElement;
+const button = document.querySelector("button") as HTMLButtonElement;
+const destroy = document.querySelector(".unsubscribe") as HTMLButtonElement;
+const todoList = document.querySelector(".todos") as HTMLLIElement;
+
+const reducers = {
+  todos: fromStore.reducer,
+};
+
+const store = new fromStore.Store(reducers);
 
 button.addEventListener(
-  'click',
+  "click",
   () => {
     if (!input.value.trim()) return;
 
     const payload = { label: input.value, complete: false };
 
-    console.log(payload);
+    store.dispatch({
+      type: fromStore.ADD_TODO,
+      payload: payload,
+    });
+    // store.dispatch(new fromStore.AddTodo(payload)) <- this not working
 
-    input.value = '';
+    console.log(store.value);
+
+    input.value = "";
   },
   false
 );
 
-todoList.addEventListener('click', function(event) {
+const unsubscribe = store.subscrible((state) => {
+  renderTodos(state.todos.data);
+});
+
+destroy.addEventListener("click", unsubscribe, false);
+
+todoList.addEventListener("click", function (event) {
   const target = event.target as HTMLButtonElement;
-  if (target.nodeName.toLowerCase() === 'button') {
-    console.log(target);
+  if (target.nodeName.toLowerCase() === "button") {
+    const todo = JSON.parse(target.getAttribute("data-todo") as any);
+    store.dispatch({
+      type: fromStore.REMOVE_TODO,
+      payload: todo,
+    });
+
+    // store.dispatch(new fromStore.RemoveTodo(todo));
   }
 });
+
+store.subscrible((state) => console.log("STATE: ", state));
